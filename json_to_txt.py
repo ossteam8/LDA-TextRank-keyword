df04616c229f3ac876eb83ec0cbd7f64eba0b989
heejae <linkyouhj@gmail.com>
2021-05-19 18:35:35 +0900

    json_to_txt and predict modified

--- json_to_txt.py
+++ json_to_txt.py
@@ -7,23 +7,28 @@ with open(path1,'r') as f:
 
 txt_file=open("news_2020_ver1.txt",'w')
 
+txt_2 = open("news_2020_ver1_2.txt",'w')
+
 #디렉토리 내의 json 파일만 인식해서 처리
 
 n = 0
-l = len(json_data["document"])
+#l = len(json_data["document"])
+#article_len =
 sents = {}
 #print(json_data["document"][0]["paragraph"])
 for i in json_data["document"]:
     for j in i["paragraph"]:
         if n not in sents:
             sents[n] = []
-            sents[n] = str(j["form"] + "  ")
+            sents[n] = str(j["form"] + "\n")
         else :
-            sents[n] = str(sents[n] + j["form"] + "  ")
-    sents[n] = str(sents[n]+ "\n")
+            sents[n] = str(sents[n] + j["form"] + "\n")
+    sents[n] = str(sents[n]+ "\n\n")
     n += 1
 
 for i in sents:
-    txt_file.write(sents[i])
+    txt_2.write(sents[i])
 
 txt_file.close()
+txt_2.close()
+
