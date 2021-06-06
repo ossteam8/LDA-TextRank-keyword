e525f8dfb807d379d74a341631db2f47e1a84f1b
heejae <linkyouhj@gmail.com>
2021-06-06 18:54:00 +0900

    test

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ class LDA_TR:
             for j in range(len(topic_docs_save[i])):
                 top_save[topic_docs_save[i][j][0]] = topic_docs_save[i][j][1]
             ext_topic_cluster[i] = [keywords[i - 1], top_save]
-        return sorted(ext_topic_cluster[i][0],reverse=True, key = lambda x: len(x[2])), NUM_TOPICS
+        return ext_topic_cluster, NUM_TOPICS
 
 
 
@@ -38,29 +38,34 @@ class LDA_TR:
 
 
 def run():
-    freeze_support()
-    st = time.time()
-    with open('sample_data/economy_id.pickle', 'rb') as f:
-        id_news = pickle.load(f)
-    with open('sample_data/politic_id.pickle', 'rb') as f:
-        id_news.extend(pickle.load(f))
-    with open('sample_data/society_id.pickle', 'rb') as f:
-        id_news.extend(pickle.load(f))
+    for n in range(5):
+        freeze_support()
+        st = time.time()
+        with open('sample_data/economy_id.pickle', 'rb') as f:
+            id_news = pickle.load(f)
+        with open('sample_data/politic_id.pickle', 'rb') as f:
+            id_news.extend(pickle.load(f))
+        with open('sample_data/society_id.pickle', 'rb') as f:
+            id_news.extend(pickle.load(f))
 
-    with open('sample_data/economy_contents.pickle', 'rb') as f:
-        news = pickle.load(f)
-    with open('sample_data/politic_contents.pickle', 'rb') as f:
-        news.extend(pickle.load(f))
-    with open('sample_data/society_contents.pickle', 'rb') as f:
-        news.extend(pickle.load(f))
-    lda_tr = LDA_TR()
-    etc, num = lda_tr.save_topics(news,id_news)
-    print('loop')
-    print(num)
-    print(time.time()-st)
-    print(len(news))
-    for i in range(1,num+1):
-        print(sorted(etc[i][0],reverse=True, key = lambda x: len(x[2])))
+        with open('sample_data/economy_contents.pickle', 'rb') as f:
+            news = pickle.load(f)
+        with open('sample_data/politic_contents.pickle', 'rb') as f:
+            news.extend(pickle.load(f))
+        with open('sample_data/society_contents.pickle', 'rb') as f:
+            news.extend(pickle.load(f))
+        lda_tr = LDA_TR()
+        etc, num = lda_tr.save_topics(news,id_news)
+        print('loop')
+        print(num)
+        print(time.time()-st)
+        print(len(news))
+        save_file = [[]]
+        for i in range(1,num+1):
+            save_file.append(sorted(etc[i][0],reverse=True, key = lambda x: len(x[2])))
+            print(etc[i][0])
+        with open("data%d.pickle"% n, "wb") as fw:
+            pickle.dump(save_file, fw)
 
 if __name__ == '__main__':
     run()
